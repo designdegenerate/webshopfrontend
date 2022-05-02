71cfd57deccf58516e7b9b1f765c84a4792c9274
BNargiz <nargiz493@gmail.com>
2022-05-02 16:41:02 +0200

    icons added plus minor css

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,28 +1,41 @@
+import { FaRegUser } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import { FaRegHeart } from "react-icons/fa";
+
 const { Link } = require("react-router-dom");
+
 const NavBar = () => {
   return (
     <nav>
-      <div>
-        <ul>
-          <li>
-            <img src="" alt="TheShop"></img>
-          </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Products">Products</Link>
-          </li>
-          <li>
-            <Link to="/Blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/Contact">Contact</Link>
-          </li>
-        </ul>
-      </div>
+      <ul>
+        <li>
+          <img src="" alt="TheShop"></img>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Products">Products</Link>
+        </li>
+        <li>
+          <Link to="/Blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+      </ul>
       <input type="text"></input>
-      <div><p>hello</p></div>
+      <div id="btnRow">
+        <Link to="/User">
+          <FaRegUser />
+        </Link>
+        <Link to="/Cart">
+          <FiShoppingCart />
+        </Link>
+        <Link to="/Favorite">
+          <FaRegHeart />
+        </Link>
+      </div>
     </nav>
   );
 };
