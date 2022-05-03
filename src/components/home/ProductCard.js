f602ed0ae2f3058ca49712b40b18d73aa36943e2
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-03 11:50:44 +0200

    feat/split_componentCard

---
+++ src/components/home/ProductCard.js
@@ -0,0 +1,25 @@
+const ProductCard = () => {
+  return (
+    <div className="productCard">
+      <img height="300" width="300" alt="" src=""></img>
+      <div className="productCardContent">
+        <h2>Dictum Morbi</h2>
+        <div>
+          <p>$26.00</p>
+          <p> * * * *</p>
+        </div>
+        <p>
+          Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget
+          urna mollis ornare vel eu leo.
+        </p>
+        <div>
+          <p>AddToCart</p>
+          <p>Favorite</p>
+          <p>Zoom</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/components/home/ProductList.js
+++ src/components/home/ProductList.js
@@ -1,26 +1,10 @@
+import ProductCard from "./ProductCard";
+
 const ProductList = () => {
   return (
     <section className="productList">
-
-      <div className="productCard">
-        <img height="300" width="300" alt="" src=""></img>
-        <div className="productCardContent">
-          <h2>Dictum Morbi</h2>
-          <div>
-            <p>$26.00</p>
-            <p> * * * *</p>
-          </div>
-          <p>
-            Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus
-            eget urna mollis ornare vel eu leo.
-          </p>
-          <div>
-            <p>AddToCart</p>
-            <p>Favorite</p>
-            <p>Zoom</p>
-          </div>
-        </div>
-      </div>
+      <ProductCard />
+      <ProductCard />
     </section>
   );
 };
