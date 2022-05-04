bbbf4bb47713e081c3883d84567e49d97a090d0a
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-04 10:07:13 +0200

    feat/homepage_category_html

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,15 +3,13 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const Homepage = () => {
-  const [ getProducts, setProducts ] = useState([]);
+  const [getProducts, setProducts] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
       let response;
       try {
-        response = await axios.get(
-          "http://localhost:4000/products"
-        );
+        response = await axios.get("http://localhost:4000/products");
       } catch (error) {
         console.log(error);
       }
@@ -26,17 +24,32 @@ const Homepage = () => {
         <div>
           <h2>Categories</h2>
           <ul>
-            <li>PrestaShop</li>
-            <li>PrestaShop</li>
-            <li>PrestaShop</li>
+            <li>
+              <input id="catElectronics" type="checkbox"></input>
+              <label for="catElectronics">Electronics</label>
+            </li>
+            <li>
+              <input id="catJewelery" type="checkbox"></input>
+              <label for="catJewelery">Jewelery</label>
+            </li>
+            <li>
+              <input id="catMenClothing" type="checkbox"></input>
+              <label for="catMenClothing">Men's Clothing</label>
+            </li>
+            <li>
+              <input id="catWomenClothing" type="checkbox"></input>
+              <label for="catWomenClothing">Women's Clothing</label>
+            </li>
           </ul>
         </div>
         <div>
-          <h2>Rating Item</h2>
+          <h2>Rating</h2>
           <ul>
             <li>five stars</li>
             <li>four stars</li>
             <li>three stars</li>
+            <li>two stars</li>
+            <li>one star</li>
           </ul>
         </div>
       </section>
