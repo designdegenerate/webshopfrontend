f1eeb689d57b15268aab649f340cd441fb9f7c58
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-04 13:40:43 +0200

    feat/home_integrate_topSect_and_footer

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,8 @@
 import ProductList from "../components/home/ProductList";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Footer from "../components/footer";
+import Topsection from "../components/topSection";
 
 const Homepage = () => {
   const [getProducts, setProducts] = useState([]);
@@ -34,54 +36,58 @@ const Homepage = () => {
   };
 
   return (
-    <article id="homePage">
-      <section className="sidebar">
-        <div>
-          <h2>Categories</h2>
-          <ul>
-            <li>
-              <input
-                id="catElectronics"
-                type="checkbox"
-                onChange={() => {
-                  filterProducts(1);
-                }}
-              ></input>
-              <label for="catElectronics">Electronics</label>
-            </li>
-            <li>
-              <input
-                id="catJewelery"
-                type="checkbox"
-                onChange={() => {
-                  filterProducts(2);
-                }}
-              ></input>
-              <label for="catJewelery">Jewelery</label>
-            </li>
-            <li>
-              <input id="catMenClothing" type="checkbox"></input>
-              <label for="catMenClothing">Men's Clothing</label>
-            </li>
-            <li>
-              <input id="catWomenClothing" type="checkbox"></input>
-              <label for="catWomenClothing">Women's Clothing</label>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h2>Rating</h2>
-          <ul>
-            <li>five stars</li>
-            <li>four stars</li>
-            <li>three stars</li>
-            <li>two stars</li>
-            <li>one star</li>
-          </ul>
-        </div>
-      </section>
-      <ProductList products={getProducts} />
-    </article>
+    <div id="homePage">
+      <Topsection />
+      <div id="homePageInner">
+        <section className="sidebar">
+          <div>
+            <h2>Categories</h2>
+            <ul>
+              <li>
+                <input
+                  id="catElectronics"
+                  type="checkbox"
+                  onChange={() => {
+                    filterProducts(1);
+                  }}
+                ></input>
+                <label for="catElectronics">Electronics</label>
+              </li>
+              <li>
+                <input
+                  id="catJewelery"
+                  type="checkbox"
+                  onChange={() => {
+                    filterProducts(2);
+                  }}
+                ></input>
+                <label for="catJewelery">Jewelery</label>
+              </li>
+              <li>
+                <input id="catMenClothing" type="checkbox"></input>
+                <label for="catMenClothing">Men's Clothing</label>
+              </li>
+              <li>
+                <input id="catWomenClothing" type="checkbox"></input>
+                <label for="catWomenClothing">Women's Clothing</label>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Rating</h2>
+            <ul>
+              <li>five stars</li>
+              <li>four stars</li>
+              <li>three stars</li>
+              <li>two stars</li>
+              <li>one star</li>
+            </ul>
+          </div>
+        </section>
+        <ProductList products={getProducts} />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
