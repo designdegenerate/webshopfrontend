af63b1411703b9d525c7623de66a03e7ebe9a60b
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-05 13:25:03 +0200

    feat/home/implement-feature

--- src/components/home/ProductList.js
+++ src/components/home/ProductList.js
@@ -1,11 +1,32 @@
 import ProductCard from "./ProductCard";
 
 const ProductList = (props) => {
+  const isFiltered = props.filter.find((cat) => cat.checked === true);
+
+  //Creates an array of filters where checked=true.
+  let filteredFilters;
+
+  const filterTheFilters = () => {
+    if(!isFiltered) return filteredFilters = [{id: true}];
+
+    filteredFilters = props.filter.filter( filter => filter.checked === true);
+  };
+
+  filterTheFilters();
 
   return (
     <section className="productList">
-      {props.products.map( (product) => {
-        return <ProductCard key={product.id} details={product} />
+      {
+      props.products
+      .filter( (product) => {
+
+        if (
+          filteredFilters.find(filter => filter.id == product.categoryId)
+        ) return product
+
+      })
+      .map((product) => {
+        return <ProductCard key={product.id} details={product} />;
       })}
     </section>
   );
